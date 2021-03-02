b481600587493d03731dd8b27203a454ebf87d28
BlokeAble <bcaballero54@gmail.com>
2021-03-01 17:29:09 -0700

    Node.js py to html

--- FrontEnd/Pages/index.js
+++ FrontEnd/Pages/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {spawn} = require('child_process');
 const expressSession = require('express-session');
 const bodyParser = require('body-parser');
 
@@ -9,5 +10,24 @@ let urlencodedParser = bodyParser.urlencoded( {
 const app = express();
 app.post('/createClothing', urlencodedParser, routes.createFood);
 app.post('/createAccount', urlencodedParser, routes.createAccount);
+app.get('/', (req, res) => {
+    
+    var dataToSend;
+    // spawn new child process to call the python script
+    //const python = spawn('python', ['script1.py']);
+    const python = spawn('python', ['script2.py','node.js','python']);
+    // collect data from script
+    python.stdout.on('data', function (data) {
+        console.log('Pipe data from python script ...');
+        dataToSend = data.toString();
+    });
+    // in close event we are sure that stream from child process is closed
+    python.on('close', (code) => {
+        console.log(`child process close all stdio with code ${code}`);
+        // send data to browser
+        res.send(dataToSend)
+    });
+    
+})
 
 app.listen(3000);
