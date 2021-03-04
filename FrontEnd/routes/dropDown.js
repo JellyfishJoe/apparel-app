afd19164b4a0693762cbbd86b314408356cd30d2
AdamG-23 <74260999+AdamG-23@users.noreply.github.com>
2021-03-04 10:15:01 -0700

    Dropdown button logic

---
+++ FrontEnd/routes/dropDown.js
@@ -0,0 +1,10 @@
+function drop() {
+    document.getElementById("signInDrop").classList.toggle("show");
+}
+//enables the dropdown button to show content
+
+// window.onclick = function(event) {
+//     document.getElementById("signInDrop").classList.toggle("hide");
+// }
+//allows when clicked away from the button hides the content
+//not yet implemented
