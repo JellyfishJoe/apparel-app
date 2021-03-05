8b075351de4cf5d0b2525310e9e3de54b6328337
BlokeAble <bcaballero54@gmail.com>
2021-03-04 17:06:00 -0700

    Fixing merges

--- BackEnd/Test.py
+++ BackEnd/Test.py
@@ -1,10 +1,25 @@
 from flask import request, redirect
 
+
+@app.route("/")
+def home():
+    resp = flask.Response("Foo bar baz")
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+
+
 @app.route('/wardrobe', methods = ['POST'])
 def signup():
+    resp = flask.Response("Foo bar baz")
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+
     image = request.form['rawImage']
     print("The image" + image + "'")
-    return redirect('/detail')
+     #insert color py
+     
+    return resp
+
+
 
 @app.route('/wardrobe', methods = ['POST'])
 
